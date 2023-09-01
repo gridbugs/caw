09f7e4f03a5fb8a4798b89c4249e8f6da7e17764
Stephen Sherratt <stephen@sherra.tt>
2023-09-02 00:56:03 +1000

    Rename default window title

--- interactive/src/window.rs
+++ interactive/src/window.rs
@@ -94,7 +94,7 @@ impl WindowBuilder {
 
     pub fn build(self) -> Window {
         Window {
-            title: self.title.unwrap_or_else(|| "Ibis".to_string()),
+            title: self.title.unwrap_or_else(|| "Ibis Synthesizer".to_string()),
             width_px: self.width_px.unwrap_or(960),
             height_px: self.height_px.unwrap_or(720),
             stable: self.stable.unwrap_or(false),
