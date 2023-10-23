20b4ebdd1f905df34e59ca476989c40923d64544
Stephen Sherratt <stephen@sherra.tt>
2023-10-23 23:40:22 +1100

    Add some effects to monophonic synth

--- interactive/examples/midi_live_with_serial_monophonic.rs
+++ interactive/examples/midi_live_with_serial_monophonic.rs
@@ -9,10 +9,13 @@ struct Effects {
     lfo_scale: Sf64,
     detune: Sf64,
     echo_scale: Sf64,
+    sah_freq: Sf64,
+    sah_scale: Sf64,
+    slide: Sf64,
 }
 
 impl Effects {
-    fn new(modulation: Sf64, extra_controllers: &MidiControllerTable) -> Self {
+    fn new(controllers: &MidiControllerTable, extra_controllers: &MidiControllerTable) -> Self {
         Self {
             high_pass_filter_cutoff: extra_controllers.get_01(35).exp_01(1.0),
             low_pass_filter_cutoff: extra_controllers.get_01(31).inv_01().exp_01(1.0),
@@ -20,7 +23,10 @@ impl Effects {
             lfo_freq: extra_controllers.get_01(33),
             lfo_scale: extra_controllers.get_01(34),
             detune: extra_controllers.get_01(36),
-            echo_scale: modulation,
+            echo_scale: controllers.modulation(),
+            sah_freq: controllers.get_01(21),
+            sah_scale: controllers.get_01(22),
+            slide: controllers.get_01(23),
         }
     }
 }
@@ -44,9 +50,12 @@ fn make_voice(
         lfo_scale,
         detune,
         echo_scale,
-    } = Effects::new(controllers.modulation(), serial_controllers);
-    let velocity_01 = velocity_01.filter(low_pass_butterworth(10.0).build());
-    let note_freq_hz = sfreq_to_hz(note_freq).filter(low_pass_butterworth(10.0).build())
+        sah_freq,
+        sah_scale,
+        slide,
+    } = Effects::new(controllers, serial_controllers);
+    let velocity_01 = velocity_01.exp_01(-2.0);
+    let note_freq_hz = sfreq_to_hz(note_freq).filter(low_pass_butterworth(slide * 20.0).build())
         * pitch_bend_multiplier_hz.into();
     let waveform = Waveform::Saw;
     let osc = mean([
@@ -75,12 +84,19 @@ fn make_voice(
         .build()
         .signed_to_01()
         * lfo_scale;
+    let sah_clock = periodic_gate_s(sah_freq + 0.01)
+        .build()
+        .to_trigger_rising_edge();
+    let sah = noise()
+        .signed_to_01()
+        .filter(sample_and_hold(sah_clock.clone()).build());
     let filtered_osc = osc
         .filter(
             low_pass_moog_ladder(sum([
                 200.0.into(),
                 &env * (low_pass_filter_cutoff * 10_000.0),
                 lfo * 10_000.0,
+                (sah * sah_scale * 5000),
             ]))
             .resonance(low_pass_filter_resonance * 4.0)
             .build(),
@@ -117,7 +133,7 @@ enum Commands {
         midi_port: usize,
         #[arg(short, long)]
         serial_port: String,
-        #[arg(short, long, default_value_t = 115200)]
+        #[arg(long, default_value_t = 115200)]
         serial_baud: u32,
     },
 }
