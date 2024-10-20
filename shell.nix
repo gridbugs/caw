2e97f23c10e9bb1d88638140658c04345b8c5809
Stephen Sherratt <stephen@sherra.tt>
2024-10-20 23:59:11 +1100

    Nix fmt

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
-{
-  pkgs ? import <nixpkgs> {
-    overlays = [(import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))];
-  }
-}:
+{ pkgs ? import <nixpkgs> {
+  overlays = [
+    (import (builtins.fetchTarball
+      "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+  ];
+} }:
 
 pkgs.mkShell rec {
   packages = with pkgs; [
